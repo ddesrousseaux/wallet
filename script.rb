92921e85184f52bd7d843506e8d4c73c1315b9a4
Damien Desrousseaux <damien@awe.fr>
2010-04-19 11:51:15 +0200

    Exercice 3

--- script.rb
+++ script.rb
@@ -1,7 +1,22 @@
 require 'devise.rb'
 
-d= Devise.new
-d.exchange_rate = 0.88
-d.name = "Euro"
 
-puts d
+euro= Devise.new
+euro.name= "euro"
+euro.exchange_rate= 1.3471
+
+yen= Devise.new
+yen.name= "yen"
+yen.exchange_rate= 0.010868
+
+pound= Devise.new
+pound.name= "pound"
+pound.exchange_rate= 1.5259
+
+devises = {
+  euro.name => euro,
+  yen.name => yen,
+  pound.name => pound,
+}
+
+puts devises
